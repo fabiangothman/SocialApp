6d61456a3c1fdc4220cb7323401dd419197c4b86
fabiangothman <fabianmurillo.01@gmail.com>
2021-04-14 17:04:49 -0500

    Adding general AuthStack style

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -6,69 +6,76 @@ import OnboardingScreen from '../screens/OnboardingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import AsyncStorage from '@react-native-community/async-storage';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
-  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
-  let routeName;
+    const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+    let routeName;
 
-  useEffect(() => {
-    AsyncStorage.getItem('alreadyLaunched').then(value => {
-      if(value == null){
-        AsyncStorage.setItem('alreadyLaunched', 'true');
-        setIsFirstLaunch(true);
-      }else{
-        setIsFirstLaunch(false);
-      }
-    });
-  }, []);
-  
-  if(isFirstLaunch === null){
-    return null;
-  }else if (isFirstLaunch === true){
-    routeName = 'Onboarding';
-  }else{
-    routeName = 'Login';
-  }
+    useEffect(() => {
+        AsyncStorage.getItem('alreadyLaunched').then(value => {
+        if(value == null){
+            AsyncStorage.setItem('alreadyLaunched', 'true');
+            setIsFirstLaunch(true);
+        }else{
+            setIsFirstLaunch(false);
+        }
+        });
+    }, []);
+    
+    if(isFirstLaunch === null){
+        return null;
+    }else if (isFirstLaunch === true){
+        routeName = 'Onboarding';
+    }else{
+        routeName = 'Login';
+    }
 
-  return (
-      <Stack.Navigator initialRouteName={routeName}>
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{header: () => null}}
-          />
-          <Stack.Screen
-            name="Onboarding"
-            component={OnboardingScreen}
-            options={{header: () => null}}
-          />
-          <Stack.Screen
-            name="Signup"
-            component={SignupScreen}
-            options={({navigation}) => ({
-                title: "",
-                headerStyle: {
-                    backgroundColor: "#f9fafd",
-                    shadowColor: "f9fafd",
-                    elevation: 0
-                },
-                headerLeft: () => (
-                    <View>
-                        <FontAwesome.Button
-                        name="long-arrow-left"
-                        size={25}
-                        backgroundColor="#f9fafd"
-                        color="#333"
-                        onPress={() => navigation.navigate("Login")} />
-                    </View>
-                )
-            })}
-          />
-      </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator styles={styles.baseStyle} initialRouteName={routeName}>
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{header: () => null}}
+            />
+            <Stack.Screen
+                name="Onboarding"
+                component={OnboardingScreen}
+                options={{header: () => null}}
+            />
+            <Stack.Screen
+                name="Signup"
+                component={SignupScreen}
+                options={({navigation}) => ({
+                    title: "",
+                    headerStyle: {
+                        backgroundColor: "#f9fafd",
+                        shadowColor: "f9fafd",
+                        elevation: 0
+                    },
+                    headerLeft: () => (
+                        <View>
+                            <FontAwesome.Button
+                            name="long-arrow-left"
+                            size={25}
+                            backgroundColor="#f9fafd"
+                            color="#333"
+                            onPress={() => navigation.navigate("Login")} />
+                        </View>
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
 };
 export default AuthStack;
+
+const styles = StyleSheet.create({
+    baseStyle: {
+        borderWidth: 1,
+        borderColor: "#ffffff"
+    }
+});
