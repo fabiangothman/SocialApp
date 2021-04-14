1b8fb7239b1b613bb57685f8f39a8b5464bf6ee8
fabiangothman <fabianmurillo.01@gmail.com>
2021-04-14 12:17:31 -0500

    Creating onboarding navigation

--- App.js
+++ App.js
@@ -6,19 +6,41 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import OnboardingScreen from './screens/OnboardingScreen';
 import LoginScreen from './screens/LoginScreen';
+import { useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const AppStack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator headerMode="none">
-        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-        <AppStack.Screen name="Login" component={LoginScreen} />
-      </AppStack.Navigator>
-      
-      <StatusBar style="auto" />
-    </NavigationContainer>
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value == null){
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      }else{
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if(isFirstLaunch === null){
+    return null;
+  }else if (isFirstLaunch === true){
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+          <AppStack.Screen name="Login" component={LoginScreen} />
+        </AppStack.Navigator>
+        
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    );
+  }else{
+    return <LoginScreen />
+  }
+
 }
 export default App;
--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,13 +1,70 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, Image, StyleSheet} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Onboarding from 'react-native-onboarding-swiper';
+
+const Skip = ({...props}) => {
+    return (
+        <Button title='Skip' color="#000000" />
+    );
+};
+const Next = ({...props}) => {
+    return (
+        <Button title='Next' color="#000000" {...props} />
+    );
+};
+const Done = ({...props}) => {
+    return (
+        <TouchableOpacity style={{marginHorizontal:10}} {...props}>
+            <Text style={{fontSize:16}}>Done</Text>
+        </TouchableOpacity>
+    );
+};
+const Dots = ({selected}) => {
+    let backgroundColor;
+    backgroundColor= selected ? "rgba(0, 0, 0, 0.8)": "rgba(0, 0, 0, 0.3)";
+    return (
+        <View style={{
+            width:5,
+            height:5,
+            marginHorizontal:3,
+            backgroundColor
+        }} />
+    );
+};
 
 const OnboardingScreen = ({navigation}) => {
     return (
-        <View style={styles.container}>
-            <Text>Onboarding Screen</Text>
-            <Button title="Click here"
-                onPress={() => navigation.navigate("Login")} />
-        </View>
+        <Onboarding
+            SkipButtonComponent={Skip}
+            NextButtonComponent={Next}
+            DoneButtonComponent={Done}
+            DotComponent={Dots}
+            //onSkip={() => navigation.navigate("Login")}
+            onSkip={() => navigation.replace("Login")}
+            onDone={() => navigation.navigate("Login")}
+            pages={[
+                {
+                    backgroundColor: '#a6e4d0',
+                    image: <Image source={require('../assets/onboarding-img1.png')} />,
+                    title: 'Connect to the world',
+                    subtitle: 'A new way to connect with the world',
+                },
+                {
+                    backgroundColor: '#fdeb93',
+                    image: <Image source={require('../assets/onboarding-img2.png')} />,
+                    title: 'Share your favorites',
+                    subtitle: 'Share your thoughts with similar kind of people',
+                },
+                
+                {
+                    backgroundColor: '#e9bcbe',
+                    image: <Image source={require('../assets/onboarding-img3.png')} />,
+                    title: 'Become the star',
+                    subtitle: 'Let the spot light capture you',
+                }
+            ]}
+        />
     );
 };
 export default OnboardingScreen;
